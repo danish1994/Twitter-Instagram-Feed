a61ca2272a9c5052e28bb845636c18137fe313cf
Danish Kamal <danish8802204230@gmail.com>
2017-01-04 01:43:48 +0530

    Gitignore Updated

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+var key = 'bKorpbiyXEP06XYX62qiFDiAC'
+var secret = 'tZBAvZ07xJ0ZajudQPOy0Oj0pCeoFeCuMdx6P9UEpBoUFKEqAl'
+var bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOVvygAAAAAAob21OGJMB9idQeF2Hv0mfdjsIQM%3DWbv6F1mBkFEiNhwXuA0rXfYdEHXbwqVMzkAglGe2sEaNxjmqEJ'
+
+module.exports = {
+	key: key,
+	secret: secret,
+	bearer_token: bearer_token
+}
