c6e46e707b3a2497cfd6b8d07eb01a3537feea27
Danish Kamal <danish8802204230@gmail.com>
2017-01-04 23:23:14 +0530

    Insta Add

--- keys.js
+++ keys.js
@@ -2,8 +2,8 @@
 var tw_key = 'bKorpbiyXEP06XYX62qiFDiAC'
 var tw_secret = 'tZBAvZ07xJ0ZajudQPOy0Oj0pCeoFeCuMdx6P9UEpBoUFKEqAl'
 var tw_bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOVvygAAAAAAob21OGJMB9idQeF2Hv0mfdjsIQM%3DWbv6F1mBkFEiNhwXuA0rXfYdEHXbwqVMzkAglGe2sEaNxjmqEJ'
-var ig_key = 'aa95a4c1e60d4badbae38e65eeaaf9a8'
-var ig_secret = '4a4f7bb62dd64ffca4792927fbeac66a'
+var ig_key = '81d45319b24649a2813cde4ed611fab3'
+var ig_secret = '4808692aa7484ba7b1a5eb9f57bb367f'
 
 module.exports = {
 	tw_key: tw_key,
